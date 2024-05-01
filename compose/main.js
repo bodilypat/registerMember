abf74a68355f1017d4cc02daf2718366d4fcd7cb
bodilypat <139784676+bodilypat@users.noreply.github.com>
2024-04-30 22:55:55 -0700

    Create main.js

---
+++ compose/main.js
@@ -0,0 +1,11 @@
+'user strict'
+var main= function() {
+  var $html = $('html'), $win = $(window), wrap = $('app-aside), MEDIAQUERY = {}, app = $('#app');
+      MEDIAQUERY = {
+           desktopXL = 1200,
+           desktop :992,
+           tablet : 768,
+           mobile : 480,
+    };
+    $("#.current-year").text(new Date()).getFullYear());
+  
